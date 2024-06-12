74d4f68d63b14d0b47c48d655c6b53e0c4bd07c7
thomazllr <guithomaz270@gmail.com>
2024-06-12 13:45:31 -0300

    from work

---
+++ cmake-build-debug/CMakeFiles/CircularLinkedList.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/08184638132/Documents/uft/algorithms/CircularLinkedList/CircularLinkedList.c" "CMakeFiles/CircularLinkedList.dir/CircularLinkedList/CircularLinkedList.c.obj" "gcc" "CMakeFiles/CircularLinkedList.dir/CircularLinkedList/CircularLinkedList.c.obj.d"
+  "C:/Users/08184638132/Documents/uft/algorithms/CircularLinkedList/main.c" "CMakeFiles/CircularLinkedList.dir/CircularLinkedList/main.c.obj" "gcc" "CMakeFiles/CircularLinkedList.dir/CircularLinkedList/main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
